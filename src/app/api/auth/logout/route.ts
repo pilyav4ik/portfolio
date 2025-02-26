0d58e1611f977a82b09c54eb3954bc9827d2d59d
Igor Piliavskyi <ipiliavskyi@gmail.com>
2025-02-26 11:29:37 +0100

    remove admin

--- src/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  // Удаляем токен, устанавливая куку с истекшим сроком действия
-  const response = NextResponse.json({ message: "Выход выполнен" });
-  response.cookies.set("token", "", { maxAge: 0, path: "/" });
-
-  return response;
-}
