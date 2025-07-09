84e49da6052157a05d054dc20f0e30205c3358c5
Igor Piliavskyi <ipiliavskyi@gmail.com>
2025-07-09 12:20:01 +0200

    #1 Remove Prisma from project

--- src/app/components/projectsOnHome/index.tsx
+++ src/app/components/projectsOnHome/index.tsx
@@ -1,6 +1,5 @@
 import Projects from "@/app/projects/projects.client";
 import AllProjectsLink from "../allProjectsLink";
-import prisma from "@/app/lib/db";
 import { createClient } from "@/app/utils/supabase/server";
 
 
@@ -13,15 +12,6 @@ export default async function ProjectsOnHome() {
   const { data: project } = await supabase.from("projects").select('*').order('id', { ascending: false })
   .limit(2);
 
-  {/*
-    const project = await prisma.project.findMany({
-    take: 2,
-    include: {
-      services: {
-      }
-    }
-  });
-   */}
 
   return (<>
         <Projects projects={project || []} services={[]} />
--- src/app/services/projectService.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '@/app/lib/db';
-import { Project } from '@prisma/client';
-
-export async function updateProject(id: number, updatedData: Partial<Project>) {
-  return await prisma.project.update({
-    where: { id },
-    data: updatedData,
-  });
-}
